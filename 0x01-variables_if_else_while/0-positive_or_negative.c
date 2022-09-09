ad9f1300ceb9fce02de55c338429b9ec1a72bde4
Kuyik2 <kbzoe2@gmail.com>
2022-09-09 13:32:53 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,18 +14,19 @@
          srand(time(0)); 
          n = rand() - RAND_MAX / 2; 
   
-         printf("Last digit of %d is %d ", n, n % 10); 
-         if (n % 10 > 5) 
+         if (n == 0) 
          { 
-                 printf("and is greater than 5\n"); 
+                 printf("%d is zero\n", n); 
          } 
-         else if (n % 10 == 0) 
+  
+         else if (n < 0) 
          { 
-                 printf("and is 0\n"); 
+                 printf("%d is negative\n", n); 
          } 
-         else if (n % 10 < 6 && n % 10 != 0) 
+  
+         else 
          { 
-                 printf("and is less than 6 and not 0\n"); 
+                 printf("%d is positive\n", n); 
          } 
          return (0); 
  }
