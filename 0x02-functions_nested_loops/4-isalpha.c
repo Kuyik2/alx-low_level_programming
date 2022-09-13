a3eeb93a3654bf6f4fa96e6eeac2d1943b94985c
Kuyik2 <kbzoe2@gmail.com>
2022-09-13 10:49:42 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -12,8 +12,8 @@
  {
          if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
          {
-                 return (1);
+                return (1);
          }
-         return (0);
+         return (0):
  }
 
