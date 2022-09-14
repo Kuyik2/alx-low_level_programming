af86c724ec86f7f3947370440bdb82cdff5f9e1a
Kuyik2 <kbzoe2@gmail.com>
2022-09-14 07:19:56 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-times_table -> time table
+*times_table -> time table
 */
 void times_table(void)
 {
@@ -19,7 +19,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-			}else if (k >= 10)
+			} else if (k >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
