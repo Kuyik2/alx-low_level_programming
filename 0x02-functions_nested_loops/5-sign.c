6737038b7e5c6d51246bceaa73511077430cd589
Kuyik2 <kbzoe2@gmail.com>
2022-09-14 06:34:54 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 
 /**
 *print_sign -> prints sign based on condition
+*@n: argument passed
 *Return: 1, 0 -1
 */
 int print_sign(int n)
@@ -22,4 +23,5 @@ int print_sign(int n)
 		return (-1);
 	}
 }
+
 
