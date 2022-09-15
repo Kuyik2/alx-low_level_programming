ed19ca2e9db2de4afd68066ff18ba49ad9a3530e
Kuyik2 <kbzoe2@gmail.com>
2022-09-15 14:32:48 -0700

    tasl 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,21 +1,17 @@
 #include "main.h"
 
 /**
-*main - prints putchar
-*Return: always 0
+*main - tests function that prints if integer is positive or negative
+Return: 0
 */
+
 int main(void)
 {
-     _putchar('_');
-     _putchar('p');
-     _putchar('u');
-     _putchar('t');
-     _putchar('c');
-     _putchar('h');
-     _putchar('a');
-     _putchar('r');
-     _putchar('\n');
-
-     return (0)
+	int i;
+		
+	i = 0;
+	positive_or_negative(i);
+	
+	return (0);
 }
 
