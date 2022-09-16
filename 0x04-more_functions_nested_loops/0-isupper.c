daed4a7fe3c6050e11864a979d3b0b0b5ac1decf
Kuyik2 <kbzoe2@gmail.com>
2022-09-15 18:50:48 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
-* _isupper -> this is a function to check upper case alphas
-* @c: an integer paramer
-* Return: something but success
+*_isupper -> this is a function to check upper case alphas
+*@c: an integer paramer
+*Return: something but success
 */
 int _isupper(int c)
 {
