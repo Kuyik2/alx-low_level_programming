c18773a95e3f4012291666761f4d3b16d47d8c76
Kuyik2 <kbzoe2@gmail.com>
2022-09-15 18:19:53 -0700

    task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,5 @@
-#ifindef main_h
-#define main_h
-
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -10,9 +9,8 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
-void print_squar(int size);
+void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif /* main.h */
-
+#endif
