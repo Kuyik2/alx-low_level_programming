078b118a4b7f75bc847f1ae655cdeaf554a695f4
Kuyik2 <kbzoe2@gmail.com>
2022-09-19 03:00:36 -0700

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,19 +1,15 @@
 #include "main.h"
 
 /**
-*swap-function that swap two integers
-*@n: parameter
-*Description-swap two integers of *a and *b to 98 and 42
+*swap_int-swaps the values of two integers
+*@a: integer to swap
+*@b: integer to swap
 */
-void swap_int(int *a, int *b);
-{
-int a;
-int b;
 
-a = 98;
-b = 42;
-printf("a=%d, b=%d\n", a, b);
-swap_int(&a, &b);
-printf("a=%d, b=%d\n", a, b);
-return (0);
+void swap_int(int *a, int *b)
+{
+  int hold;
+  hold = *a;
+  *a = *b;
+  *b = hold;
 }
